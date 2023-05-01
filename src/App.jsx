78c8a0f1b4831274992e67bd629a5c029e61e96d
Ikem <cliroaganachi@gmail.com>
2023-05-01 17:11:13 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='my-auto p-4 mx-auto text-red-600 text-center'>
+        <h1>
+          Ikem
+        </h1>
+     </div>
+    </>
+  )
+}
+
+export default App
